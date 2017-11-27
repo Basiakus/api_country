eef30e46d4c7d7c349be3902c1c500412c0e805f
Adam Basiak <basiakus@gmail.com>
2017-11-27 20:13:28 +0100

    change with jq methods

--- js/script.js
+++ js/script.js
@@ -17,8 +17,19 @@ function showCountriesList(resp) {
     countriesList.empty();
 
     resp.forEach(function(item) {
-        $('<li>').text("Kraj: " + item.name + " Stolica: " + item.capital +
-            " Populacja: " + item.population + " Kierunkowy: " + item.callingCodes).appendTo(countriesList);
-       $(item.name).addClass('item');
+
+    var countryLi = $('<li><p><span>Kraj: </span></p></li>');
+    $('#countries').append(countryLi).append(item.name);
+
+    var capitaLi = $('<li><p><span>Stolica: </span></p></li>');
+    $('#countries').append(capitaLi).append(item.capital);
+
+    var popuLi = $('<li><p><span>Populacja: </span></p></li>');
+    $('#countries').append(popuLi).append(item.population);
+
+    var calLi = $('<li><p><span>Kierunkowy: </span></p></li>');
+    $('#countries').append(calLi).append(item.callingCodes);
+    
+
     });
 };
